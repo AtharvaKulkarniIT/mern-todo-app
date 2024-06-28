108c38aac2bf223baf79f4c12da473cdcc9e6c79
Atharva Kulkarni <122991006+AtharvaKulkarniIT@users.noreply.github.com>
2024-06-28 09:55:15 +0530

    Update Home.js

--- TODO/todo_frontend/src/Home.js
+++ TODO/todo_frontend/src/Home.js
@@ -2,54 +2,96 @@ import React, { useEffect, useState } from 'react';
 import Create from './Create';
 import './App.css';
 import axios from 'axios';
-import { BsCircleFill,BsFillCheckCircleFill,BsFillTrashFill } from 'react-icons/bs';
+import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill, BsPencil } from 'react-icons/bs';
 
 const Home = () => {
-    const [todos,setTodos]=useState([]);
-    
-    useEffect(()=>{
+    const [todos, setTodos] = useState([]);
+    const [updatetask, setUpdatetask] = useState('');
+    const [taskid, setTaskid] = useState('');
+
+    useEffect(() => {
         axios.get('http://localhost:5000/get')
-        .then(result=> setTodos(result.data))
-        .catch(err=>console.log(err))
-    },[])
+            .then(result => setTodos(result.data))
+            .catch(err => console.log(err));
+    }, []);
 
-    const edit = (id)=>{
+    const edit = (id) => {
         axios.put(`http://localhost:5000/edit/${id}`)
-        .then(result=>console.log(result.data),
-        window.location.reload() )
-        .catch(err=>console.log(err))
-
+            .then(result => {
+                console.log(result.data);
+                const updatedTodos = todos.map(todo => {
+                    if (todo._id === id) {
+                        return { ...todo, done: !todo.done };
+                    }
+                    return todo;
+                });
+                setTodos(updatedTodos);
+            })
+            .catch(err => console.log(err));
     };
 
-    const Hdelete = (id)=>{
+    const Update = (id, updatedTask) => {
+        axios.put(`http://localhost:5000/update/${id}`, { task: updatedTask })
+            .then(result => {
+                console.log(result.data);
+                const updatedTodos = todos.map(todo => {
+                    if (todo._id === id) {
+                        return { ...todo, task: updatedTask };
+                    }
+                    return todo;
+                });
+                setTodos(updatedTodos);
+                setTaskid('');
+                setUpdatetask('');
+                Window.location.reload();
+            })
+            .catch(err => console.log(err));
+    };
+
+    const Hdelete = (id) => {
         axios.delete(`http://localhost:5000/delete/${id}`)
-        .then(result=>console.log(result.data),
-        window.location.reload() )
-        .catch(err=>console.log(err))
+            .then(result => {
+                console.log(result.data);
+                const updatedTodos = todos.filter(todo => todo._id !== id);
+                setTodos(updatedTodos);
+            })
+            .catch(err => console.log(err));
+    };
 
-    }
-  return (
-    <main>
-        <Create/>
-           {
-           todos.length === 0 ? <div className='task'>No tasks found</div>
-            :todos.map((todo)=>
-               <div className='task' key={todo._id}>
-                    <div className='checkbox'  onClick={()=>edit(todo._id)} >
-                        {todo.done ? <BsFillCheckCircleFill className='icon'/>:<BsCircleFill className='icon'/>}
-                        <p className={todo.done ? 'through' :  'normal' }>{todo.task}</p>
-                    </div>
-                    <div onClick={()=>Hdelete(todo._id)}>
-                        <span><BsFillTrashFill className='icon' /></span>
-                    </div>
-                </div> 
-              )
-                
-           
-}
-    </main>
-    
-  )
-}
+    return (
+        <main>
+            <Create />
+            {
+                todos.length === 0 ? <div className='task'>No tasks found</div> :
+                    todos.map((todo) => (
+                        <div className='task' key={todo._id}>
+                            <div className='checkbox'>
+                                {todo.done ? <BsFillCheckCircleFill className='icon' /> :
+                                    <BsCircleFill className='icon' onClick={() => edit(todo._id)} />}
+                                {taskid === todo._id ?
+                                    <input type='text' value={updatetask} onChange={e => setUpdatetask(e.target.value)} />
+                                    :
+                                    <p className={todo.done ? 'through' : 'normal'}>{todo.task}</p>
+                                }
+                            </div>
+                            <div>
+                                <span>
+                                    <BsPencil className='icon' onClick={() => {
+                                        if (taskid === todo._id) {
+                                            Update(todo._id, updatetask);
+                                        } else {
+                                            setTaskid(todo._id);
+                                            setUpdatetask(todo.task);
+                                        }
+                                    }} />
+                                    <BsFillTrashFill className='icon' onClick={() => Hdelete(todo._id)} />
+                                </span>
+                            </div>
+                        </div>
+                    ))
+            }
+        </main>
+    );
+};
 
-export default Home
+export default Home;
